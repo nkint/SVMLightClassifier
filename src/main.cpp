b02d0b75076ebe9f7d3a7914a0483dc476ecc6fa
nkint <nootropic.kint@gmail.com>
2014-02-13 15:07:48 +0100

    code refactor and fixes

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,28 @@
 #include <iomanip>
 #include <sstream>
 #include <ctime>
-
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
-
 #include "SvmLightLib.h"
 
 #define TRAINING_SET_PATH "/Users/alberto/tmp/samples/"
+#define FILE_VERBOSE true
 
 using namespace cv;
-using namespace std;
+
+const Size sampleSize(48,48);
 
 void train()
 {
-    clock_t start_clock = clock();
-    time_t start_time = time(0);
 
     SVMLight::SVMTrainer svm("features.dat");
 
     HOGDescriptor hog;
-    hog.winSize = Size(50,50);
+    hog.winSize = Size(sampleSize);
 
     size_t posCount = 0, negCount = 0;
-    for (size_t i = 1; i <= 1800; ++i)
+    for (size_t i = 1; i <= 800; ++i)
     {
         // in this concrete case I had files 0001.JPG to 0800.JPG in both "positive" and "negative" subfolders:
         std::ostringstream os;
@@ -33,10 +31,10 @@ void train()
 
         Mat img = imread(os.str(),CV_LOAD_IMAGE_GRAYSCALE);
         if (!img.data) {
-            std::cout << "problems.. " << os.str() << std::endl;
+            if(FILE_VERBOSE) std::cout << "problems.. " << os.str() << std::endl;
             continue;
         } else {
-            std::cout << "read: " << os.str() << std::endl;
+            if(FILE_VERBOSE)std::cout << "read: " << os.str() << std::endl;
         }
 
         // obtain feature vector:
@@ -56,10 +54,10 @@ void train()
         os << TRAINING_SET_PATH << "negative/" << std::setw(4) << std::setfill('0') << i << ".png";
         img = imread(os.str(),CV_LOAD_IMAGE_GRAYSCALE);
         if (!img.data) {
-            std::cout << "problems.. " << os.str() << std::endl;
+            if(FILE_VERBOSE)std::cout << "problems.. " << os.str() << std::endl;
             continue;
         } else {
-            std::cout << "read: " << os.str() << std::endl;
+            if(FILE_VERBOSE)std::cout << "read: " << os.str() << std::endl;
         }
 
         hog.compute(img, featureVector, Size(8, 8), Size(0, 0));
@@ -75,37 +73,65 @@ void train()
     svm.trainAndSaveModel(modelName);
     std::cout   << "SVM saved to " << modelName << std::endl;
 
-    clock_t end_clock = clock();
-    double time_clock = (double) (end_clock-start_clock) / CLOCKS_PER_SEC * 1000.0;
-    time_t end_time = time(0);
-    double time_time = difftime(end_time, start_time) * 1000.0;
-    std::cout << "everything has been done in: " << std::endl
-              << "    " << time_clock << " CPU clock" << std::endl
-              << "    " << time_time << " milliseconds" << std::endl
-              << std::endl;
+
 }
 
-int main()
+void classify()
 {
-
-    train();
-
     HOGDescriptor hog;
-    hog.winSize = Size(50,50);
+    hog.winSize = Size(sampleSize);
     SVMLight::SVMClassifier c("classifier.dat");
     vector<float> descriptorVector = c.getDescriptorVector();
     std::cout << descriptorVector.size() << std::endl;
     hog.setSVMDetector(descriptorVector);
 
-//    Mat m = imread("/Users/alberto/tmp/samples/fullframe.png");
+    Mat m = imread("/Users/alberto/tmp/samples/fullframe.png");
 
-//    vector<Rect> found;
-//    Size padding(Size(0, 0));
-//    Size winStride(Size(8, 8));
-//    hog.detectMultiScale(m, found, 0.0, winStride, padding, 1.01, 0.1);
+    vector<Rect> found;
+    Size padding(Size(0, 0));
+    Size winStride(Size(48, 48));
 
-//    std::cout << "found: " << found.size() << std::endl;
+    std::cout << "try to detect.." << std::endl;
+    hog.detectMultiScale(m, found, 0.0, winStride, padding, 1.01, 0.1);
+    std::cout << "found: " << found.size() << std::endl;
+}
 
+int main()
+{
+    clock_t start_clock, end_clock;
+    time_t start_time, end_time;
+    double time_clock, time_time;
+
+    std::cout << std::endl
+              << "-------------------- train"
+              << std::endl;
+    start_clock = clock();
+    start_time = time(0);
+    train();
+    end_clock = clock();
+    time_clock = (double) (end_clock-start_clock) / CLOCKS_PER_SEC * 1000.0;
+    end_time = time(0);
+    time_time = difftime(end_time, start_time) * 1000.0;
+    std::cout << "done in: " << std::endl
+              << "    " << time_clock << " CPU clock" << std::endl
+              << "    " << time_time << " in time" << std::endl
+              << std::endl;
+
+
+    std::cout << std::endl
+              << "-------------------- classify"
+              << std::endl;
+    start_clock = clock();
+    start_time = time(0);
+    classify();
+    end_clock = clock();
+    time_clock = (double) (end_clock-start_clock) / CLOCKS_PER_SEC * 1000.0;
+    end_time = time(0);
+    time_time = difftime(end_time, start_time) * 1000.0;
+    std::cout << "done in: " << std::endl
+              << "    " << time_clock << " CPU clock" << std::endl
+              << "    " << time_time << " in time" << std::endl
+              << std::endl;
 
     return 0;
 }
